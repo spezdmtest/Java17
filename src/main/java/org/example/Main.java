869e1e53198214007f49f435afe314c5eed7f9c6
spezdmtest <spezd.m.@gmail.com>
2024-01-03 17:41:17 +0200

    Java 17 new features

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        PatternMatchingForSwitch.doDemoOne(1L);
+        PatternMatchingForSwitch.doDemoOne("Hello, World!");
+        PatternMatchingForSwitch.doDemoOne(new Object());
+        PatternMatchingForSwitch.doDemoTwo(new Scanner(System.in));
+          PatternMatchingForSwitch.doDemoThree();
+          PatternMatchingForSwitch.doDemoFour();
+    }
+}
---
+++ src/main/java/org/example/PatternMatchingForSwitch.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class PatternMatchingForSwitch {
+    static void doDemoOne(Object o) {
+        switch (o) {
+            case String s -> System.out.println("String: " + s);
+            case Integer i -> System.out.println("Integer: " + i);
+            case Long l -> System.out.println("Long: " + l);
+            default -> System.out.println(o);
+        }
+    }
+
+    static void doDemoTwo(Object o) {
+        switch (o) {
+            case String s -> System.out.println("String: " + s);
+            case Integer i -> System.out.println("Integer: " + i);
+            case Long l -> System.out.println("Long: " + l);
+            case Scanner scanner -> System.out.println("Scanner: " + scanner);
+            default -> System.out.println(o);
+        }
+    }
+
+    static void doDemoThree() {
+        Object o = null;
+        switch (o) {
+            case null -> System.out.println("Maybe NPE");
+            default -> System.out.println(o);
+        }
+    }
+
+    static void doDemoFour() {
+        Object o = "Str";
+
+        switch(o) {
+            case String s -> System.out.println("String: " + s);
+            case CharSequence cs -> System.out.println("String: " + cs);
+            default -> System.out.println("Default: " + o);
+        }
+    }
+
+
+}
